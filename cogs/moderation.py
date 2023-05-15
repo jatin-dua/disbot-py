00416f91067d943bb43f50e539fb916c8d4650ef
Jatin Dua <jdua004@gmail.com>
2023-05-15 22:22:44 +0530

    add mute and unmute fn

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -23,5 +23,33 @@ class ModerationCog(commands.Cog):
         else:
             await ctx.send('You do not have permission to ban members.')
 
+    @commands.command(name='mute')
+    async def mute_command(self, ctx: commands.Context, member: discord.Member, *, reason=None):
+        if ctx.author.guild_permissions.mute_members:
+            muted_role: discord.Role = discord.utils.get(ctx.guild.roles, name="muted")
+
+            if not muted_role:
+                muted_role = await ctx.guild.create_role(name="muted")
+                for channel in ctx.guild.channels:
+                    await channel.set_permissions(muted_role, send_messages=False)
+
+            await member.add_roles(muted_role)
+            await ctx.send(f'{member.mention} has been muted .')
+
+        else:
+            await ctx.send('You do not have permission to mute members.')
+
+    @commands.command(name='unmute')
+    async def unmute_command(self, ctx: commands.Context, member: discord.Member):
+        muted_role: discord.Role = discord.utils.get(ctx.guild.roles, name="muted")
+
+        if ctx.author.guild_permissions.mute_members:
+            await member.remove_roles(muted_role)
+            await ctx.send(f'{member.mention} has been unmuted .')
+
+        else:
+            await ctx.send('You do not have permission to unmute members.')
+
+
 async def setup(client: commands.Bot):
     await client.add_cog(ModerationCog(client))
