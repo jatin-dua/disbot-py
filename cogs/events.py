5a8059e06bdaa87c9daf6d828fee0fd23e262662
Jatin Dua <jdua004@gmail.com>
2023-05-17 15:10:33 +0530

    fix flood filter

--- cogs/events.py
+++ cogs/events.py
@@ -1,29 +1,18 @@
 import discord
 from discord.ext import commands
-import utils.response
-
-SPAM_LIMIT = 4
+from cogs.moderation import MODERATOR
 
 class EventHandlerCog(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
-        self.message_counts = {}
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.author == self.client.user:
             return
         
-        author_id = message.author.id
-        self.message_counts[author_id] = self.message_counts.get(author_id, 0) + 1
+        await MODERATOR.check_message_flood(message)
 
-        if self.message_counts[author_id] > SPAM_LIMIT:
-            # TODO: Store number of warns of each user and take action on them.
-            await message.reply(f"{message.author.mention} Please refrain from flooding the channel with messages.")
-
-        if 'hello' in message.content.lower():
-            await message.channel.send(utils.response.get_random_greeting())
-    
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         # Check if the member's account is less than a day old
@@ -33,6 +22,7 @@ class EventHandlerCog(commands.Cog):
             # Notify the moderators that a suspicious account has joined
             mod_channel = self.client.get_channel(1234567890) # Replace with your moderation channel ID
             await mod_channel.send(f'Suspicious account {member.mention} has joined the server.')
+
 
 async def setup(client: commands.Bot):
     await client.add_cog(EventHandlerCog(client))
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,9 +1,15 @@
 import discord
 from discord.ext import commands
+from datetime import timedelta
+
+SPAM_LIMIT = 4
+TIME = timedelta(minutes=5.0)
 
 class ModerationCog(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
+        self.message_count = {}
+        self.previous_author_id = 0
 
     @commands.command(name='kick')
     async def kick_command(self, ctx, member: discord.Member, *, reason=None):
@@ -50,6 +56,27 @@ class ModerationCog(commands.Cog):
         else:
             await ctx.send('You do not have permission to unmute members.')
 
+    async def check_message_flood(self, message: discord.Message):
+        # print(f"""
+        # Previous Author ID: {self.previous_author_id}
+        
+        # Message Author ID: {message.author.id}
+        # Message Author: {message.author}
+        #     """)
+        
+        self.message_count[message.author.id] = self.message_count.get(message.author.id, 1) + 1
+
+        if self.previous_author_id != message.author.id:
+            self.previous_author_id = message.author.id
+            del self.message_count[self.previous_author_id]
+
+        if self.message_count[message.author.id] > SPAM_LIMIT:
+            # TODO: Store number of warns of each user and take action on them.
+            await message.reply(f"{message.author.mention} Please refrain from flooding the channel with messages.")
+            await message.author.timeout(TIME)
+
 
 async def setup(client: commands.Bot):
-    await client.add_cog(ModerationCog(client))
+    global MODERATOR
+    MODERATOR = ModerationCog(client)
+    await client.add_cog(MODERATOR)
